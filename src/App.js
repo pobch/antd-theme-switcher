4b7ef0ba1130d58d10ff4548dcbbb1e070520ed2
Pob <pob@zipmex.com>
2021-06-17 23:47:28 +0700

    Approach 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { Button, Typography, Card, Layout } from 'antd'
+// import './App.less'
+import './App.light.less'
+import './App.dark.less'
 
 function App() {
+  const [isLight, setIsLight] = useState(true)
+
+  useEffect(() => {
+    if (!isLight) {
+      document.querySelector('body').style.backgroundColor = '#000'
+    } else {
+      document.querySelector('body').style.backgroundColor = 'white'
+    }
+  }, [isLight])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={isLight ? 'pob-light' : 'pob-dark'}>
+      <Layout>
+        <Layout.Header>Web App</Layout.Header>
+        <Layout>
+          <Layout.Sider>Menu</Layout.Sider>
+          <Layout.Content>
+            <Button type="primary" onClick={() => setIsLight(!isLight)}>
+              Switch Theme
+            </Button>
+            <Typography.Title level={1}>Some Heading Text</Typography.Title>
+            <Typography.Paragraph>lorum ipsum</Typography.Paragraph>
+            <Card title="Default size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
+              <p>Card content</p>
+              <p>Card content</p>
+              <p>Card content</p>
+            </Card>
+          </Layout.Content>
+        </Layout>
+      </Layout>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
